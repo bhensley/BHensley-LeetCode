52075e9649723249913508f901a1180909f66da8
Bob Hensley <bob@hnsly.us>
2023-09-27 20:30:46 -0400

    Add project files.

---
+++ 1480 - Running Sum of 1D Array/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public static int[] RunningSum(int[] nums)
+    {
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        GC.Collect();
+        return nums;
+    }
+    
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[] { 1, 1, 1, 1, 1 };
+        int[] runSum = RunningSum(nums);
+        
+        foreach(int i in runSum)
+        {
+            System.Console.WriteLine(i);    
+        }
+    }   
+}
---
+++ 1672 - Richest Customer Wealth/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        int max = 0;
+
+        for (int i = 0; i < accounts.Length; i++)
+        {
+            int cWealth = 0;
+
+            foreach (int j in accounts[i])
+            {
+                cWealth += j;
+            }
+
+            if (cWealth > max)
+            {
+                max = cWealth;
+            }
+        }
+
+        return max;
+    }
+}
