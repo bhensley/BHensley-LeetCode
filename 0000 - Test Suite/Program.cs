bad18f4c0a2c86a96c580964d6ad00bf1456ac9c
Bob Hensley <bob@hnsly.us>
2023-09-27 20:45:48 -0400

    Ditched test suite idea for now. Fizz Buzz done.

--- 0000 - Test Suite/Program.cs
+++ 0000 - Test Suite/Program.cs
@@ -1,9 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-public class TestSuite
+﻿public class Programn
 {
     public static void Main(string[] args)
     {
         // Add the project being worked on as a dependency, instantiate the class, and call the method
         // Solution solution = new Solution(); Blah blah blah
+        LeetCode.Solution fb = new LeetCode.Solution();
+        IList<string> fbOut = fb.FizzBuzz(3);
+        foreach (string s in fbOut)
+        {
+            Console.WriteLine(s);
+        }
     }
-}   
+}
---
+++ 412 - Fizz Buzz/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode
+{
+    public class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> fbOut = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                fbOut.Add(
+                    (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" :
+                        (i % 3 == 0) ? "Fizz" :
+                            (i % 5 == 0) ? "Buzz" :
+                                i.ToString()
+                );
+            }
+            return fbOut;
+        }
+    }
+}
